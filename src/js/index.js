110d9f79b8c47af2b1f4e7b8a244c52c906ea5fc
jonorl <39122230+jonorl@users.noreply.github.com>
2024-11-05 18:34:15 +0300

    Working on the IF statement

--- src/js/index.js
+++ src/js/index.js
@@ -4,24 +4,45 @@ import "../css/style.css";
 
 const imageContainer = document.querySelectorAll(".image-container img")
 const nextButton = document.querySelector(".next")
+let imagesArray = []
+
 // Event listeners
 
-nextButton.addEventListener("click", nextImage)
+for (let image of imageContainer){
+    imagesArray.push(image)
+}
 
+function indexImages(arr) {
+    arr.forEach((image, index) => {    
+        image.setAttribute(`data-image-number`, index)
+    })
+}
 
-function nextImage() {
+indexImages(imagesArray);
 
-    for (let image of imageContainer){
-        let token = false
-
-        if (image.style.display === "none" && token === false) {
-            image.style.display = "block";
-            token = true;
-        }
-        else {
-            image.style.display = "none";
-            token = false;
+function nextImage (){
+    for (let index = 0; index < (imagesArray.length - 1); index++) {
+        let nextImage;
+        let currentImage = document.querySelector(`.number[data-image-number="${index}"]`);
+        let computedStyle = window.getComputedStyle(currentImage);
+        if (computedStyle.display === "block" && imagesArray[index] !== (imagesArray.length - 2)){
+            console.log("condition 1")
+            console.log("index: " + index)
+            console.log("ImgArrayLen - 1: " + (imagesArray.length - 1))
+            let nextIndex = index + 1
+            nextImage = document.querySelector(`.number[data-image-number="${nextIndex}"]`);
+            currentImage.style.display = "none";
+            nextImage.style.display = "block";
             break;
         }
-    };
+        else if (computedStyle.display === "block" && imagesArray[index] === (imagesArray.length - 1)){
+            console.log("condition 2")
+            nextImage = document.querySelector(`.number[data-image-number="0"]`);
+            currentImage.style.display = "none";
+            nextImage.style.display = "block"
+            break;
+        }
+    }
 }
+
+nextButton.addEventListener("click", nextImage)
